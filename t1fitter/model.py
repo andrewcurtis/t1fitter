1bba6b68fe2de528a00fb26127e4fc72354bbbcf
Andrew Curtis <andrew.curtis@gmail.com>
2015-12-02 19:44:44 -0500

    Adding option parsing

--- t1fitter/model.py
+++ t1fitter/model.py
@@ -1,71 +1,103 @@
-"""
+'''
 model.py
 
-Functions to model the T1 behaviour of our data.
-SPGR, VFA, and eMOS.
+    Functions to model the T1 behaviour of our data.
+    SPGR, VFA, and eMOS.
 
 
-"""
+    Model functions for signal equation.
+    Using numexpr as a fast way to speed things up.  Numexpr will automatically multi-
+    thread numpy array computations, in addition to avoiding intermediate arrays.
+    
+    
+    Likely want to move to numba in future (or C).
+    
+    
+    TODO: test performance of more complex expressions...
+        e.g. if I don't break cos/sin/exp out as separate evals, should save som inter-
+            mediate work space
+        - Figure out nice way of generalizing which model to use.
+        - Handling of complex data?
+        - alloc space for deriv once and pass in?
+'''
+
 
 import numpy as np
 import numexpr as ne
 
 
+class T1Models(object):
+    """docstring for T1Models"""
+    def __init__(self):
+        super(T1Models, self).__init__()
+        
+    
+    @staticmethod
+    def spgr(flips, m, t1, b1, tr):
+
+        ca = ne.evaluate('cos(b1*flips)')
+        sa = ne.evaluate('sin(b1*flips)')
+        e1 = ne.evaluate('exp(-tr/t1)')
+        sig = ne.evaluate('m * sa * (1-e1)/(1-ca*e1)')
+
+        return sig
+
+    # flatten SPGR into a single numexpr call to try and avoid intermediates
+    @staticmethod
+    def spgr_flat(flips, m, t1, b1, tr):
+
+        return ne.evaluate('m * sin(b1*flips) * (1-exp(-tr/t1)) / (1-cos(b1*flips) * exp(-tr/t1))')
+
+    @staticmethod
+    def spgr_deriv_mt(flips, m, t1, b1, tr):
+    
+        e1 = ne.evaluate('exp(-tr/t1)')
+        e1p = ne.evaluate('exp(tr/t1)')
+        ca = ne.evaluate('cos(b1*flips)')
+        sa = ne.evaluate('sin(b1*flips)')
+
+        dm = ne.evaluate('sa * (1-e1)/(1-ca*e1)')
+        dt1 = ne.evaluate('((ca-1) * m*sa * tr * e1p)/(t1**2 * (ca-e1p)**2)')
+
+        return np.array([dm, dt1])
+ 
+    @staticmethod       
+    def spgr_deriv_m(flips, m, t1, b1, tr):
+    
+        e1 = ne.evaluate('exp(-tr/t1)')
+        e1p = ne.evaluate('exp(tr/t1)')
+        ca = ne.evaluate('cos(b1*flips)')
+        sa = ne.evaluate('sin(b1*flips)')
+
+        dm = ne.evaluate('sa * (1-e1)/(1-ca*e1)')
+
+        return np.array([dm])
+        
+    @staticmethod
+    def spgr_deriv_t(flips, m, t1, b1, tr):
+    
+        e1 = ne.evaluate('exp(-tr/t1)')
+        e1p = ne.evaluate('exp(tr/t1)')
+        ca = ne.evaluate('cos(b1*flips)')
+        sa = ne.evaluate('sin(b1*flips)')
+
+        dt1 = ne.evaluate('((ca-1) * m*sa * tr * e1p)/(t1**2 * (ca-e1p)**2)')
+
+        return np.array([ dt1])
 
 
-'''
-    Model functions for signal equation, and objective and jacobian for fitting
-'''
+    @staticmethod        
+    def spgr_deriv_mtb(flips, m, t1, b1, tr):
+    
+        e1 = ne.evaluate('exp(-tr/t1)')
+        e1p = ne.evaluate('exp(tr/t1)')
+        ca = ne.evaluate('cos(b1*flips)')
+        sa = ne.evaluate('sin(b1*flips)')
 
-def spgr_ne(flips, m, t1, b1, tr):
-    b1.shape = (1,-1)
-    flips.shape = (-1,1)
-    m.shape = (1,-1)
-    t1.shape= (1,-1)
-
-    ca = ne.evaluate('cos(b1*flips)')
-    sa = ne.evaluate('sin(b1*flips)')
-    e1 = ne.evaluate('exp(-tr/t1)')
-    sig = ne.evaluate('m * sa * (1-e1)/(1-ca*e1)')
-
-    return sig
-
-
-def spgr_deriv_ne(flips, m, t1, b1, tr):
-
-    b1.shape = (1,-1)
-    flips.shape = (-1,1)
-    m.shape = (1,-1)
-    t1.shape= (1,-1)
-
-    e1 = ne.evaluate('exp(-tr/t1)')
-    e1p = ne.evaluate('exp(tr/t1)')
-    ca = ne.evaluate('cos(b1*flips)')
-    sa = ne.evaluate('sin(b1*flips)')
-
-    dm = ne.evaluate('sa * (1-e1)/(1-ca*e1)')
-    dt1 = ne.evaluate('((ca-1) * m*sa * tr * e1p)/(t1**2 * (ca-e1p)**2)')
-
-    return np.array([dm, dt1])
-
-
-def spgr_deriv_ne_b1(flips, m, t1, b1, tr):
-
-    flips.shape = (-1,1)
-    b1.shape= (1,-1)
-    m.shape = (1,-1)
-    t1.shape= (1,-1)
-
-    e1 = ne.evaluate('exp(-tr/t1)')
-    e1p = ne.evaluate('exp(tr/t1)')
-    ca = ne.evaluate('cos(b1*flips)')
-    sa = ne.evaluate('sin(b1*flips)')
-
-    dm = ne.evaluate('sa * (1-e1)/(1-ca*e1)')
-    dt1 = ne.evaluate('((ca-1) * m*sa * tr * e1p)/(t1**2 * (ca-e1p)**2)')
-
-    db1 =  ne.evaluate('flips * m* (e1 - 1)*(e1 - ca) / (e1*ca - 1)**2')
-
-    tmp = np.array([dm, dt1, db1])
-
-    return tmp
+        dm = ne.evaluate('sa * (1-e1)/(1-ca*e1)')
+        dt1 = ne.evaluate('((ca-1) * m*sa * tr * e1p)/(t1**2 * (ca-e1p)**2)')
+        db1 =  ne.evaluate('flips * m * (e1 - 1)*(e1 - ca) / (e1*ca - 1)**2')
+        
+        return np.array([dm, dt1, db1])
+    
+    
--- t1fitter/t1fitter.py
+++ t1fitter/t1fitter.py
@@ -7,7 +7,7 @@ Reads image volumes and speficications, set up problem, runs, and saves output.
 
 
 import numpy as np
-
+import argparse
 from traits.api import HasTraits, Float, List, Int, Array, Double, Directory
 
 class T1Fitter(HasTraits):
@@ -29,10 +29,10 @@ class T1Fitter(HasTraits):
     t1_range = Array
     m0_range = Array
 
+    #remember affine matrix for output 
     base_image_affine = Array
 
-    def __init__(self):
-        pass
+
 
     def load_file_list(self, files):
         pass
@@ -55,10 +55,72 @@ class T1Fitter(HasTraits):
     def write_nifti(self):
         pass
 
-    def init_from_cmd_line(self):
+    def init_from_cli(self):
         pass
 
     def init_traits_gui(self):
         pass
 
+
+
+
+def t1fit_cli():
+
+    parser = argparse.ArgumentParser(description='T1fitter. VFA, eMOS, and NLReg, with optional preprocessing.')
+    parser.add_argument('--addvol', '-a', nargs=3,  action='append', metavar=('vol','flip','tr'),
+                        help='Add volume for fitting with flip angle (deg) and tr (ms)')
+
+
+    parser.add_argument('--out', '-o', default='t1fit',
+                        help='Output volume base name for t1 and m0 fit.')
+                    
+    parser.add_argument('--preproc', action="store_true",
+                        help='Run preprocessing on input volumes (alignment, brain extraction, cropping).')
+                    
+    parser.add_argument('--crop_padding', type=int, default=4, 
+                        help='Edge padding for minimum volume crop size.')
+
+    parser.add_argument('--maskvol',  
+                        help='Brain mask volume (must be provided if no preprocessing is used).')
+      
+    parser.add_argument('--b1vol', 
+                        help='B1 map (as relative scaling of base FAs).')
+ 
+    parser.add_argument('--b1smooth', type=float, default=25.0,
+                        help='smoothing factor for b1')
+                    
+    parser.add_argument('--mosvol', nargs=2,  action='append', metavar=('vol','flip'),
+                        help='Add volume for MOS B1 map calculations with associated flip angle (deg)')
+
+
+
+                    
+    #fitting options
+
+    parser.add_argument('--l1', action='store_true',
+                        help='Enable Huber penalty. See  -l1lam to set penatly scaling. ')
+    parser.add_argument('--l1lam', type=float, default=1e-3,
+                        help='l1 lambda: scaling factor for Huber penalty')
+    parser.add_argument('--kern_radius', type=int, default=1, 
+                        help='Huber spatial kernel radius.')
+    parser.add_argument('--huber_scale', type=float, default=3.0, 
+                        help='Huber spatial kernel radius.')
+
+                    
+    parser.add_argument('--l2', action='store_true',
+                        help='Enable l2 Tikhonov penalty. See  -l2lam to set penatly scaling. ')
+    parser.add_argument('--l2lam', type=float, default=1e-3,
+                        help='l2 lambda: scaling factor for Tikhonov penalty')
+    parser.add_argument('--l2mode', choices=['zero','smooth_vfa'], default='zero',
+                        help='l2 Tikhonov penalty mode -- Distance from smooth prior, or zero (normal Tik). ')
+
+
+    cmd_args = parser.parse_args()
     
+    fitter = T1Fitter()
+    fitter.init_from_cli(cmd_args)
+    
+
+
+if __name__ == '__main__':
+    main()
