Working on scc. l2 prior penalty still incorrect. strange scaling

t1fitter/optim.py
@@ -77,12 +77,12 @@ class T1FitNLLSReg(T1Fit):
             self.scratch *= 0.0;
             self.scratch[self.mask_flat,:] = x
             self.to_vol(self.scratch)
-            tmp = self.params.l1_lam * self.params.hubreg.reg_func(self.scratch)
+            tmp = self.params.l1_lam * self.params.hubreg.reg_func(self.scratch) 
             self.log.info('l1 term: {}'.format(tmp))
             retval += tmp
 
         if self.params.l2_lam > 0:
-            tmp = self.params.l2_lam * self.params.l2reg.reg_func(x)
+            tmp = self.params.l2_lam * self.params.l2reg.reg_func(x[:,1]) 
             self.log.info('l2 term: {}'.format(tmp))
             retval += tmp
 
@@ -118,21 +118,21 @@ class T1FitNLLSReg(T1Fit):
             self.params.hubreg.reg_deriv(self.scratch, self.grad_scratch)
             self.to_flat(self.grad_scratch)
 
-            tmp =  self.params.l1_lam * self.grad_scratch[self.mask_flat,:]
-            self.log.info('l1 grad norm: {}'.format(np.sum(tmp**2)))
+            tmp =  self.params.l1_lam * self.grad_scratch[self.mask_flat,:] 
+            self.log.info('l1 grad norm: {}'.format(np.sqrt(np.sum(tmp**2))))
 
             self.grad -= tmp
 
 
 
         if self.params.l2_lam > 0:
-            l2dif = self.params.l2reg.reg_deriv(x)
+            l2dif = self.params.l2reg.reg_deriv(x[:,1])
             self.log.debug('l2dif shape: {}'.format(l2dif.shape))
 
-            tmp = self.params.l2_lam * l2dif
-            self.log.info('l2 grad norm: {}'.format(np.sum(tmp**2)))
+            tmp = self.params.l2_lam * l2dif        
+            self.log.info('l2 grad norm: {}'.format(np.sqrt(np.sum(tmp**2))))
 
-            self.grad += tmp
+            self.grad[:,1] += tmp
 
 
         sim = self.params.model_func(x[:,0], x[:,1],
@@ -154,7 +154,7 @@ class T1FitNLLSReg(T1Fit):
         deriv = np.sum( - (l2diff[np.newaxis, :, :] * deriv) , axis=1)
         self.log.debug('deriv shape: {}'.format(deriv.shape))
 
-        self.log.info('obj grad norm: {}'.format(np.sum(deriv**2)))
+        self.log.info('obj grad norm: {}'.format(np.sqrt(np.sum(deriv**2))))
 
         # vox X pars
         self.grad += self.obj_scale * deriv[:,:].T
@@ -187,6 +187,8 @@ class T1FitNLLSReg(T1Fit):
         #flatten
         nx = self.x0.shape[0]
         bnds = np.zeros((nx, 2))
+
+        self.nx = nx/2.0
 
         #mo
         bnds[::2,0] = 0.001

t1fitter/regularization.py
@@ -33,7 +33,7 @@ class Regularizer3D(HasTraits):
 
     """
 
-    def __init__(self, nthreads=4):
+    def __init__(self, nthreads=8):
         ne.set_num_threads(nthreads)
 
 
@@ -55,18 +55,16 @@ class TikhonovDiffReg3D(Regularizer3D):
 
     x0 = Array
 
-    def __init__(self, x0, nthreads=4):
-        self.x0 = x0
-        ne.set_num_threads(nthreads)
+    def __init__(self, x0, nthreads=8):
+        self.x0 = x0.copy()
+        #ne.set_num_threads(nthreads)
 
     def reg_func(self, x):
-        x0 = self.x0
-        return ne.evaluate('sum((x - x0)**2)')
+        return np.sqrt(np.sum((x - self.x0)**2))
 
 
     def reg_deriv(self, x):
-        x0 = self.x0
-        grad = ne.evaluate('2*(x-x0)')
+        grad = (x-self.x0)/np.sqrt(np.sum((x-self.x0)**2) )
         return grad
 
 
@@ -83,7 +81,7 @@ class ParallelHuber2ClassReg3D(Regularizer3D):
     kern_weights = Array
     hub_scale = CFloat(3.0)
 
-    def __init__(self, img_sz, nthreads=4, kern_radius=1, hub_scale=3.0):
+    def __init__(self, img_sz, nthreads=8, kern_radius=1, hub_scale=3.0):
         self.nthreads = nthreads
         self.scratch = np.zeros(img_sz)
         self.kern_sz = kern_radius