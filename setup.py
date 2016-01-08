e73ff53e22f6d193b7e11de79159cb390487500c
Andrew Curtis <andrew.curtis@gmail.com>
2016-01-08 12:36:21 -0500

    Adding missing dependancy

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ config = {
     'url': '',
     'author_email': 'andrew.curtis@gmail.com',
     'version': '0.1',
-    'install_requires': ['numpy','scipy','nose','nibabel','sh'],
+    'install_requires': ['numpy','scipy','nose','nibabel','sh','traits'],
     'name': 't1fitter',
-    'entry_points': { 
+    'entry_points': {
             'console_scripts': [
                 't1fit = t1fitter.run_t1fit:main'
             ]},
 }
 
 setup(**config)
-
